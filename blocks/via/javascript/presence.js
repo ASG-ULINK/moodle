5677a8c71ac78e8657308fa0b7f23f6aedf0dd55
unknown <kteam@lds-international.com>
2015-06-24 16:16:01 +0530

    Signed-off-by: unknown <kteam@lds-international.com>

--- blocks/via/javascript/presence.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById("id_duration");
-input.onchange = function () {
-    var duration = "";
-
-    input = document.getElementById("id_duration").value;
-    duration = Math.round(input / 2);
-    document.getElementById("id_presence").value = duration;
-};
